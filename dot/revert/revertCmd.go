3dd7892752b068d9a4dbfc0b2ef9b4ab540ff0db
DnFreddie <defnotfreddie@gmail.com>
2024-06-15 21:22:29 +0200

    Close #23, Added the descrition to the revert flag

--- dot/revert/revertCmd.go
+++ dot/revert/revertCmd.go
@@ -1,16 +1,16 @@
 package revert
 
 import (
-	"log"
-
 	"github.com/spf13/cobra"
+	"log"
 )
 
 var options bool
 var RevertCmd = &cobra.Command{
 	Use:   "revert",
-	Short: "revert",
-	Long:  "hahahh",
+	Short: "Reverets back to the specyfied previous .config cofiguration",
+	Long:  `Brings the confguration to the previouse state by deleting all the files
+	and moving back the one that existed before.It also deletes the specyfied backup directory`,
 	Run: func(cmd *cobra.Command, args []string) {
 		if cmd.Flags().Changed("date") {
 
@@ -30,6 +30,6 @@ var RevertCmd = &cobra.Command{
 
 func init() {
 
-	RevertCmd.Flags().BoolVarP(&options, "date", "d", false, "Enable debug mode")
+	RevertCmd.Flags().BoolVarP(&options, "date", "d", false, "Delete the chosen config backup")
 
 }

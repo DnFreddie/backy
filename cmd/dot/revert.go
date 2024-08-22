011bd8ff2e3aba22c0fe365c3b4d8c0f38b34970
Dnfreddie <defnotfreddie@gmai.com>
2024-08-22 23:25:17 +0200

    Close #5 Fixed the test  and revert fuction

--- cmd/dot/revert.go
+++ cmd/dot/revert.go
@@ -15,38 +15,45 @@ import (
 	"time"
 )
 
-// Reades the back_conf and Revert the schema to the previous state
-func (t *Repo)Revert() error {
-
+func (t *Repo) Revert() error {
 	backupPath, err := utils.Checkdir(BACK_CONF, false)
 	if err != nil {
-		log.Fatal("Failed to create backup")
+		return fmt.Errorf("failed to create backup: %w", err)
 	}
 
-	//TODO! Fix this read config 
-	dirs, err := os.ReadDir(path.Join(backupPath,"dotfiles"))
-	slog.Debug("This is ", "dirs",dirs)
+	dirs, err := os.ReadDir(path.Join(backupPath, "dotfiles"))
+	if err != nil {
+		return fmt.Errorf("failed to read directories: %w", err)
+	}
+	slog.Debug("Directories read:", "dirs", dirs)
 
 	bV, err := chooseBackupVersion(dirs)
 	if err != nil {
-		log.Fatal("Failed to chose the veriosn ", err)
+		return fmt.Errorf("failed to choose the version: %w", err)
 	}
-	r, err := processReversion(path.Join(backupPath,"dotfiles", bV))
 
+	r, err := processReversion(path.Join(backupPath, "dotfiles", bV))
 	if err != nil {
-
-		log.Fatal(err)
-
+		return err
 	}
-	for _, dot := range *r.Dots {
-		
+
+	for _, d := range *r.Dots {
+
+		if err := d.cleanLinks(); err != nil {
+			return err
+		}
 
 
-		dot.cleanLinks()
+		err := os.RemoveAll(path.Join(backupPath, "dotfiles", bV))
+		if err != nil {
+			return &utils.UserError{FPath: path.Join(backupPath, "dotfiles", bV),Err: err}
+		}
+
 	}
 	return nil
-
 }
+
+// Reades the back_conf and Revert the schema to the previous state
 func (d *Dotfile) cleanLinks() error {
 	if d.Symlink == "" {
 		slog.Info("No symlink to clean, skipping.")
@@ -80,6 +87,7 @@ func (d *Dotfile) cleanLinks() error {
 		return fmt.Errorf("failed to remove symlink %s: %w", d.Symlink, err)
 	}
 
+//If old (meaning it existed before)
 	src := filepath.Join(d.Repo.BackupLocation, d.Location)
 	if err := utils.Copy(src, d.Symlink); err != nil {
 		slog.Error("Failed to copy from source to symlink:", "src", src, "symlink", d.Symlink, "error", err)
@@ -88,6 +96,36 @@ func (d *Dotfile) cleanLinks() error {
 
 	slog.Info("Successfully cleaned and updated symlink:", "symlink", d.Symlink, "source", src)
 	return nil
+}
+
+func processReversion(chosenPath string) (Repo, error) {
+	r := &Repo{}
+	csvPath := path.Join(chosenPath, utils.SCHEMA_JSON)
+
+	if _, err := os.Stat(csvPath); os.IsNotExist(err) {
+		log.Fatal(&utils.UserError{Err: err, FPath: csvPath})
+	}
+
+	f, err := os.Open(csvPath)
+	if err != nil {
+		slog.Error("Failed to open file", "path", csvPath)
+		return Repo{}, err
+	}
+	defer f.Close()
+
+	if err := json.NewDecoder(f).Decode(r); err != nil {
+		log.Fatal(&utils.UserError{FPath: csvPath, Err: err})
+		return Repo{}, err
+	}
+
+	//Unmarchaling get's rid of the pointer to repo this
+	//reassign it
+	if r.Dots != nil {
+		for i := range *r.Dots {
+			(*r.Dots)[i].Repo = r
+		}
+	}
+	return *r, nil
 }
 
 func chooseBackupVersion(options []os.DirEntry) (string, error) {
@@ -118,29 +156,3 @@ func chooseBackupVersion(options []os.DirEntry) (string, error) {
 		return options[len(options)-choice].Name(), nil
 	}
 }
-
-func processReversion(chosenPath string) (Repo, error) {
-	r := &Repo{}
-	csvPath := path.Join(chosenPath, utils.SCHEMA_JSON)
-
-	if _, err := os.Stat(csvPath); os.IsNotExist(err) {
-		log.Fatal(&utils.UserError{Err: err, FPath: csvPath})
-	}
-
-	f, err := os.Open(csvPath)
-	if err != nil {
-		slog.Error("Failed to open file", "path", csvPath)
-		return Repo{}, err
-	}
-	defer f.Close() 
-
-	if err := json.NewDecoder(f).Decode(r); err != nil {
-		log.Fatal(&utils.UserError{FPath: csvPath, Err: err})
-		return Repo{}, err
-	}
-
-	return *r, nil
-}
-
-
-
--- cmd/dot/revert_test.go
+++ cmd/dot/revert_test.go
@@ -1,47 +1,84 @@
 package dot
 
 import (
-	"errors"
-	"io/fs"
+	"fmt"
+	"github.com/DnFreddie/backy/common"
+	"log/slog"
+	"os"
+	"path"
+	"path/filepath"
 	"testing"
 )
 
-func TestChoseBackupVersion(t *testing.T) {
-
-}
-
-type MockDirEntry struct {
-	name     string
-	isDir    bool
-	fileInfo fs.FileInfo
-}
-
-func NewMockDirEntry(name string, isDir bool, fileInfo fs.FileInfo) *MockDirEntry {
-	return &MockDirEntry{
-		name:     name,
-		isDir:    isDir,
-		fileInfo: fileInfo,
+func prepTest() (string, error) {
+	r := &Repo{}
+	force := true
+	target := path.Join(os.Getenv("HOME"), "backy")
+	err := r.ReadLocal(target)
+	if err != nil {
+		return "", fmt.Errorf("failed to read the local Repo %s: %w", r.Absolute, err)
 	}
-}
 
-func (m *MockDirEntry) Info() (fs.FileInfo, error) {
-	if m.fileInfo == nil {
-		return nil, errors.New("file info not set")
+	err = r.getDots()
+	if err != nil {
+		return "", fmt.Errorf("error getting paths: %w", err)
 	}
-	return m.fileInfo, nil
+
+	slog.Debug("Started Linking")
+	r.Link(force)
+	slog.Debug("Saving schema")
+	common.SaveStructure(r)
+
+	return r.BackupLocation, nil
 }
 
-func (m *MockDirEntry) IsDir() bool {
-	return m.isDir
-}
-
-func (m *MockDirEntry) Name() string {
-	return m.name
-}
-
-func (m *MockDirEntry) Type() fs.FileMode {
-	if m.fileInfo != nil {
-		return m.fileInfo.Mode()
+func TestRevert(t *testing.T) {
+	bLocation, err := prepTest()
+	if err != nil {
+		t.Fatalf("prepTest failed: %v", err)
 	}
-	return 0
+
+	baseDir := filepath.Dir(bLocation)
+	newFileName := bLocation[len(baseDir)+1:]
+
+	newPath := filepath.Join(baseDir, newFileName)
+	r, err := processReversion(newPath)
+	if err != nil {
+		t.Fatalf("processReversion failed: %v", err)
+	}
+
+	for _, d := range *r.Dots {
+		if err := d.cleanLinks(); err != nil {
+			fmt.Println(bLocation)
+			t.Errorf("failed to clean links: %v", err)
+			continue
+		}
+
+	}
+
+	// Check if the symlinks where correclt removed
+	configDir := path.Join(os.Getenv("HOME"), ".config")
+	err = filepath.Walk(configDir, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if info.Mode()&os.ModeSymlink != 0 {
+			return fmt.Errorf("found symlink: %s", path)
+		}
+		return nil
+	})
+
+	if err != nil {
+		t.Errorf("error while checking for symlinks in .config: %v", err)
+	}
+	// Check for the existence of the LICENSE file
+	licensePath := path.Join(configDir, "LICENSE")
+	if _, err := os.Stat(licensePath); os.IsNotExist(err) {
+		t.Errorf("LICENSE file does not exist in .config")
+	}
+
+	err = os.RemoveAll(bLocation)
+	if err != nil {
+		t.Errorf("failed to remove path %s: %v", bLocation, err)
+	}
 }
